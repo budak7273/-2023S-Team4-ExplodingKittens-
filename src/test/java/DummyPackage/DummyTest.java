f8f8b268db326fc88dae160966f17d24a895ca8c
rhit-daviscl <daviscl@rose-hulman.edu>
2022-03-24 18:06:00 -0400

    Fixed gradle issues

---
+++ src/test/java/DummyPackage/DummyTest.java
@@ -0,0 +1,14 @@
+package DummyPackage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    void testDummy(){
+        Assertions.assertTrue(false);
+    }
+
+
+}
