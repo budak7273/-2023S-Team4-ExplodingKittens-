b2eb5d0f08a7f3ae832842402fc936b8a16a0412
rhit-boatmaee <boatmaee@gmail.com>
2022-04-03 15:29:34 -0400

    Added test for createGame2PlayerTest- fails. Waiting to hear back from team on ambiguity in written DoD for this input option.

--- src/test/java/Presentation/PresentationTests.java
+++ src/test/java/Presentation/PresentationTests.java
@@ -1,5 +1,7 @@
 package Presentation;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +15,31 @@ public class PresentationTests {
     }
     @Test
     public void createGame2PlayerTest(){
-
+//        Gameboard gameboard = new Gameboard();
+//        Integer twoPlayers = 2;
+//        EasyMock mockedDrawDeck = EasyMock.createMock(DrawDeck.class);
+//        EasyMock mockedDiscardDeck = EasyMock.createMock(DiscardDeck.class);
+//        EasyMock mockedUser = EasyMock.createMock(User.class);
+//
+//        EasyMock.expect(mockedDrawDeckdrawDeck.size());
+//        EasyMock.expect(mockedDiscardDeckdiscardDeck.size());
+//
+//
+//        assertEquals(0, gameboard.playerQueue.size());
+//        assertEquals(0, gameboard.drawDeck.size());
+//
+//        EasyMock.expect(mockedUser.getHandSize());
+//
+//        assertEquals(0, gameboard.discardDeck.size());
+//        try {
+//            gameboard.createGame(twoPlayers);
+//        } catch (InvalidPlayerCountException e) {
+//            Assertions.fail();
+//        }
+//        assertEquals(0, gameboard.playerQueue.size());
+//        assertEquals(0, gameboard.drawDeck.size());
+//        assertEquals(0, gameboard.discardDeck.size());
+            fail("Not yet implemented");
     }
 
 
