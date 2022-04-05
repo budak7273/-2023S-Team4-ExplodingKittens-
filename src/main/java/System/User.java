2c3920a937490e6530651e6ab66dfe2decb34025
rhit-fengr <fengr@rose-hulman.edu>
2022-04-05 02:24:01 -0400

    passes testUserConstructor_AliveOrDead

--- src/main/java/System/User.java
+++ src/main/java/System/User.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 
 public class User {
     String name;
+    Boolean alive;
 
     public User(){}
 
     public User(String name, boolean alive, ArrayList<Card> hand) {
         this.name = name;
+        this.alive = alive;
     }
 }
--- src/test/java/System/UserTesting.java
+++ src/test/java/System/UserTesting.java
@@ -13,6 +13,15 @@ public class UserTesting {
         Assertions.assertEquals("test1", user.name);
         Assertions.assertNotEquals("test0", user.name);
     }
+
+    @Test
+    public void testUserConstructor_Alive(){
+        User user = new User("test1", true, new ArrayList<Card>());
+        User user2 = new User("test2", false, new ArrayList<Card>());
+        Assertions.assertTrue(user.alive);
+        Assertions.assertFalse(user2.alive);
+    }
+
     @Test
     public void testCheckForPairs_EmptyHand() {
         User user = new User();
