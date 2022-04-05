f2188f35cfca972078e89b5ce31891c6d2fe17ce
rhit-fengr <fengr@rose-hulman.edu>
2022-04-05 02:20:03 -0400

    testUserConstructor_Name passes

--- src/main/java/System/User.java
+++ src/main/java/System/User.java
@@ -1,4 +1,14 @@
 package System;
 
+
+import java.util.ArrayList;
+
 public class User {
+    String name;
+
+    public User(){}
+
+    public User(String name, boolean alive, ArrayList<Card> hand) {
+        this.name = name;
+    }
 }
--- src/test/java/System/UserTesting.java
+++ src/test/java/System/UserTesting.java
@@ -1,4 +1,23 @@
 package System;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserTesting {
+    @Test
+    public void testUserConstructor_Name(){
+        User user = new User("test1", false, new ArrayList<Card>());
+        Assertions.assertEquals("test1", user.name);
+        Assertions.assertNotEquals("test0", user.name);
+    }
+    @Test
+    public void testCheckForPairs_EmptyHand() {
+        User user = new User();
+        List<String> names = new ArrayList<>();
+//        Executable executable = () -> setup.createUsers(names);
+//        Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
 }
