da974d03bd3a5d523e796fe0cf20ad13496a79db
rhit-boatmaee <boatmaee@gmail.com>
2022-04-06 13:54:42 -0400

    Added user command line user input for usernames

--- src/main/java/Presentation/Gameboard.java
+++ src/main/java/Presentation/Gameboard.java
@@ -1,32 +1,50 @@
 package Presentation;
 
 import java.io.File;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 import System.*;
 public class Gameboard {
-    private Queue<User> users = new ArrayDeque<User>();
+    private Queue<User> users = new ArrayDeque<>();
     private DrawDeck drawDeck = new DrawDeck();
     private DiscardDeck discardDeck = new DiscardDeck();
-    public void createGame(Integer playerCount) throws InvalidPlayerCountException {
+    Scanner scanner = new Scanner(System.in);
+
+    public void createGame() throws InvalidPlayerCountException {
+
+        List<String> usernames = readUserInfo();
+        int playerCount = usernames.size();
         if(playerCount == 1){
             throw new InvalidPlayerCountException("ERROR: Must have at least 2 players!");
         }
-        List<String> usernames = readUserInfo();
-        Setup s = new Setup(usernames.size());
+        Setup s = new Setup(playerCount);
         this.users = s.createUsers(usernames);
-        this.drawDeck = s.createDrawDeck(new File(""));
+       // this.drawDeck = s.createDrawDeck(new File(""));
         this.discardDeck = s.createDiscardDeck();
 
     }
     public List<String> readUserInfo(){
-        List<String> twoUsers = new ArrayList<>();
-        twoUsers.add("player1");
-        twoUsers.add("player2");
-        return twoUsers;
+        List<String> userNameList = new ArrayList<>();
+        System.out.println("Please enter player usernames!");
+        int numberOfPlayers;
+
+            while(scanner.hasNext()){
+                String username = scanner.next();
+                userNameList.add(username);
+                System.out.println(username + " has been added to the game!");
+                System.out.println("Would you like to add another player? (y/n)");
+                String response = scanner.next();
+                boolean addAnotherPlayer = (response.equals("Y") || response.equals("y"));
+                if(!addAnotherPlayer){
+                    break;
+                } else{
+                    System.out.println("Enter next player username");
+                }
+            }
+
+
+        scanner.close();
+        return userNameList;
     }
 
     public Queue<User> getUsers() {
@@ -40,4 +58,6 @@ public class Gameboard {
     public DiscardDeck getDiscardDeck() {
         return this.discardDeck;
     }
+
+
 }
