19312c33e7df2479c1b9152c2dc61d2c354d155f
rhit-boatmaee <boatmaee@gmail.com>
2022-04-07 13:33:12 -0400

    Fixed issue with File

--- src/main/java/Presentation/Gameboard.java
+++ src/main/java/Presentation/Gameboard.java
@@ -19,30 +19,39 @@ public class Gameboard {
         }
         Setup s = new Setup(playerCount);
         this.users = s.createUsers(usernames);
-       // this.drawDeck = s.createDrawDeck(new File(""));
+        String path = "src/main/resources/cards.csv";
+        this.drawDeck = s.createDrawDeck(new File(path));
         this.discardDeck = s.createDiscardDeck();
 
     }
     public List<String> readUserInfo(){
         List<String> userNameList = new ArrayList<>();
-        System.out.println("Please enter player usernames!");
-        int numberOfPlayers;
+        System.out.println("Please enter player 1's username!");
+        int nextPlayerCount = 2;
 
             while(scanner.hasNext()){
                 String username = scanner.next();
                 userNameList.add(username);
                 System.out.println(username + " has been added to the game!");
-                System.out.println("Would you like to add another player? (y/n)");
+                if( nextPlayerCount < 11) {
+                    System.out.println("Would you like to add another player? (y/n)");
+                } else{
+                    break;
+                }
                 String response = scanner.next();
                 boolean addAnotherPlayer = (response.equals("Y") || response.equals("y"));
                 if(!addAnotherPlayer){
                     break;
-                } else{
-                    System.out.println("Enter next player username");
+
+                }else{
+                    System.out.println("Enter player " + nextPlayerCount + "'s username!");
+                    nextPlayerCount++;
                 }
             }
-
-
+        System.out.println("Starting Exploding Kittens game for players: ");
+            for(String userName: userNameList){
+                System.out.println(userName);
+            }
         scanner.close();
         return userNameList;
     }
