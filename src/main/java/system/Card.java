88d58a19ee043c58b809d06c367076beadd54b2d
Emily <boatmaee@gmail.com>
2022-04-24 16:20:28 -0400

    More checkstyle updates

--- src/main/java/system/Card.java
+++ src/main/java/system/Card.java
@@ -1,13 +1,13 @@
 package system;
 
 public abstract class Card {
-    private final String name;
+    private final String cardName;
 
-    public Card(String name) {
-        this.name = name;
+    public Card(final String name) {
+        this.cardName = name;
     }
 
     public String getName() {
-        return this.name;
+        return this.cardName;
     }
 }
--- src/main/java/system/DrawDeck.java
+++ src/main/java/system/DrawDeck.java
@@ -4,24 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DrawDeck {
-    List<Card> cards;
+   private List<Card> cards;
 
     public DrawDeck() {
-        cards = new ArrayList<Card>();
+        cards = new ArrayList<>();
     }
     public int getDeckSize() {
         return cards.size();
     }
 
-    public void addCard(Card card) {
+    public void addCard(final Card card) {
         cards.add(card);
     }
 
-    public void drawCard(User drawingUser) {
+    public void drawCard(final User drawingUser) {
         if (cards.isEmpty()) {
-            throw new RuntimeException("Draw deck is empty, the game was set up improperly.");
+            throw new RuntimeException("Draw deck is empty, "
+                    + "the game was set up improperly.");
         }
         Card drawnCard = cards.remove(0);
         drawingUser.addCard(drawnCard);
+    }
+
+    public List<Card> getCards() {
+        return this.cards;
     }
 }
