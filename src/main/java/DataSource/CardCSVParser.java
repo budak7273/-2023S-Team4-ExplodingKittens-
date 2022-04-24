e8fc9700c327f028d204339613ff1711d63a6edc
Nathaniel Blanco <blancont@rose-hulman.edu>
2022-04-24 15:14:42 -0400

    testGenerateListOfCards_None fails

--- src/main/java/DataSource/CardCSVParser.java
+++ src/main/java/DataSource/CardCSVParser.java
@@ -1,10 +1,13 @@
 package DataSource;
 
+import System.Card;
+import java.util.List;
+
 public class CardCSVParser {
     public CardCSVParser(String path) {
     }
 
-    public void generateListOfCards(boolean includePaw, boolean includeNoPaw) {
+    public List<Card> generateListOfCards(boolean includePaw, boolean includeNoPaw) {
         throw new IllegalArgumentException("Not enough cards in .csv file.");
     }
 }
--- src/test/java/DataSource/CardCSVParserTesting.java
+++ src/test/java/DataSource/CardCSVParserTesting.java
@@ -3,6 +3,9 @@ package DataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import System.Card;
+
+import java.util.List;
 
 public class CardCSVParserTesting {
     @Test
@@ -27,5 +30,13 @@ public class CardCSVParserTesting {
         CardCSVParser parser = new CardCSVParser(path);
         Executable executable = () -> parser.generateListOfCards(true, true);
         Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    public void testGenerateListOfCards_None() {
+        String path = "src/test/resources/fullfile.csv";
+        CardCSVParser parser = new CardCSVParser(path);
+        List<Card> cardList = parser.generateListOfCards(false, false);
+        Assertions.assertTrue(cardList.isEmpty());
     }
 }
