ed5906e0c53657121de6f1d23adff558440b37a5
rhit-boatmaee <64987342+rhit-boatmaee@users.noreply.github.com>
2022-04-25 01:22:21 -0400

    added CardType enum

---
+++ src/main/java/datasource/CardType.java
@@ -0,0 +1,96 @@
+package datasource;
+
+public enum CardType {
+    ATTACK {
+        public String toString() {
+            return "Attack";
+        }
+    },
+
+    EXPLODING_KITTEN {
+        public String toString() {
+            return "Exploding Kitten";
+        }
+    },
+    DEFUSE {
+        public String toString() {
+            return "Defuse";
+        }
+    },
+    SKIP {
+        public String toString() {
+            return "Skip";
+        }
+    },
+    FAVOR {
+        public String toString() {
+            return "Favor";
+        }
+    },
+    SHUFFLE {
+        public String toString() {
+            return "Shuffle";
+        }
+    },
+
+    BEARD_CAT {
+        public String toString() {
+            return "Beard Cat";
+        }
+    },
+    TACO_CAT {
+        public String toString() {
+            return "Tacocat";
+        }
+    },
+
+    HAIRY_POTATO_CAT {
+        public String toString() {
+            return "Hairy Potato Cat";
+        }
+    },
+    RAINBOW_RALPHING_CAT {
+        public String toString() {
+            return "Rainbow-Ralphing Cat";
+        }
+    },
+
+    CATTERMELON {
+        public String toString() {
+            return "Cattermelon";
+        }
+    },
+    FERAL_CAT {
+        public String toString() {
+            return "Feral Cat";
+        }
+    },
+
+    DRAW_FROM_THE_BOTTOM {
+        public String toString() {
+            return "Draw From The Bottom";
+        }
+    },
+    NOPE {
+        public String toString() {
+            return "Nope";
+        }
+    },
+
+    ALTER_THE_FUTURE {
+        public String toString() {
+            return "Alter The Future";
+        }
+    },
+    TARGETED_ATTACK {
+        public String toString() {
+            return "Targeted Attack";
+        }
+    },
+
+    SEE_THE_FUTURE {
+        public String toString() {
+            return "See The Future";
+        }
+    }
+}
