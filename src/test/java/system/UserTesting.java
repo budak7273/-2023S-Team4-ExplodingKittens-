3cb9931bebb3ef372e673be2148dbb3e128127ec
rhit-fengr <70914802+rhit-fengr@users.noreply.github.com>
2022-04-25 19:05:01 -0400

    Update UserTesting.java
    remove commented lines

--- src/test/java/system/UserTesting.java
+++ src/test/java/system/UserTesting.java
@@ -78,16 +78,6 @@ public class UserTesting {
         Assertions.assertFalse(user.checkForSpecialEffectPotential());
     }
 
-//    @Test
-//    public void testCheckForSpecialEffectPotential_TwoDifferentCard(){
-//        ArrayList<Card> list = new ArrayList<Card>();
-//        Card card = new AttackCard();
-//        list.add(card);
-//        Card card2 = new AttackCard();
-//        User user = new User("test1", false, list);
-//        Assertions.assertFalse(user.checkForSpecialEffectPotential());
-//    }
-
     @Test
     public void testVerifySpecialEffectForCardsSelected_EmptyHandWithNonEmptyList(){
         ArrayList<Card> list = new ArrayList<Card>();
