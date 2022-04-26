74c24f6e36072a1bf84f49d87f773854c5375034
Sam Polonus <78012388+rhit-polonusb@users.noreply.github.com>
2022-04-25 22:17:06 -0400

    testDistributeCards10Users passes

--- src/test/java/System/GameStateTesting.java
+++ src/test/java/System/GameStateTesting.java
@@ -105,4 +105,30 @@ public class GameStateTesting {
 
         EasyMock.verify(player1, player2, setDeck);
     }
+
+    @Test
+    public void testDistributeCards10Users() {
+        List<User> players = new ArrayList<User>();
+        for (int i=0; i<10; i++) {
+            players.add(EasyMock.createMock(User.class));
+        }
+        DrawDeck setDeck = EasyMock.createMock(DrawDeck.class);
+        for (int i=0; i<7; i++) {
+            players.forEach(setDeck::drawInitialCard);
+        }
+        EasyMock.replay(players.get(0), players.get(1), players.get(2),
+                players.get(3), players.get(4), players.get(5),
+                players.get(6), players.get(7), players.get(8),
+                players.get(9), setDeck);
+
+        Queue<User> users = new LinkedList<>(players);
+
+        GameState gameState = new GameState(users, new Gameboard());
+        gameState.dealHands(setDeck);
+
+        EasyMock.verify(players.get(0), players.get(1), players.get(2),
+                players.get(3), players.get(4), players.get(5),
+                players.get(6), players.get(7), players.get(8),
+                players.get(9), setDeck);
+    }
 }
