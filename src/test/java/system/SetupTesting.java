7d323cc5d0d2a3da142c30ef8bd7d0b0ab3b8dac
Nathaniel Blanco <blancont@rose-hulman.edu>
2022-04-28 16:48:38 -0400

    testCreateDrawDeck_fromFullFileAnd8Players passes

--- src/test/java/system/SetupTesting.java
+++ src/test/java/system/SetupTesting.java
@@ -163,7 +163,11 @@ public class SetupTesting {
         String path = "src/test/resources/fullfile.csv";
         File cardInfoFile = new File(path);
         DrawDeck drawDeck = setup.createDrawDeck(cardInfoFile);
-        Assertions.assertTrue(drawDeck.getDeckSize() == PARTY_PACK_SIZE);
+
+        int numberOfDefuseCardsToAdd = 2;
+        int expectedDeckSize = PARTY_PACK_SIZE + numberOfDefuseCardsToAdd;
+        Assertions.assertEquals(expectedDeckSize, drawDeck.getDeckSize());
+        Assertions.assertTrue(drawDeck.getDefuseCount() == numberOfDefuseCardsToAdd);
     }
 
     @Test
