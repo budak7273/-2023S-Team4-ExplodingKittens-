d4189df17d2ac09a51308e542142bd05a229a42b
Nathaniel Blanco <blancont@rose-hulman.edu>
2022-05-01 19:02:19 -0400

    checkstyle passes

--- src/main/java/System/SkipEffect.java
+++ src/main/java/System/SkipEffect.java
@@ -2,6 +2,8 @@ package system;
 
 public class SkipEffect implements EffectPattern {
     @Override
-    public void useEffect(GameState gameState) {
+    public void useEffect(final GameState gameState) {
+        gameState.transitionToNextTurn();
+        gameState.transitionToNextTurn();
     }
 }
