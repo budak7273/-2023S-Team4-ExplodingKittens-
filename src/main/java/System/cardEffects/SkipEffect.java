d730cb0b9b311b1cd1db9b7610767dfe841a1e85
rhit-boatmaee <64987342+rhit-boatmaee@users.noreply.github.com>
2022-05-02 21:37:16 -0400

    updated SkipEffect to remove final

--- src/main/java/System/cardEffects/SkipEffect.java
+++ src/main/java/System/cardEffects/SkipEffect.java
@@ -4,7 +4,7 @@ import system.GameState;
 
 public class SkipEffect implements EffectPattern {
     @Override
-    public void useEffect(final GameState gameState) {
+    public void useEffect(GameState gameState) {
         gameState.transitionToNextTurn();
         gameState.transitionToNextTurn();
     }
