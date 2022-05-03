3773c152c5a38ba1a37ceb3413f6cdd54dae2b92
Nathaniel Blanco <blancont@rose-hulman.edu>
2022-05-03 08:11:53 -0400

    testEqualsDifferentType passes

--- src/test/java/System/CardTesting.java
+++ src/test/java/System/CardTesting.java
@@ -11,4 +11,11 @@ public class CardTesting {
         Object otherObj = 1;
         Assertions.assertFalse(card.equals(otherObj));
     }
+
+    @Test
+    public void testEqualsDifferentType() {
+        Card card = new Card(CardType.ATTACK);
+        Object otherObj = new Card(CardType.ALTER_THE_FUTURE);
+        Assertions.assertFalse(card.equals(otherObj));
+    }
 }
