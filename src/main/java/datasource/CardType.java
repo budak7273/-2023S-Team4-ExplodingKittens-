b9c1825e5dcf4a66fa814a4e889f0ef159e3de28
Nathaniel Blanco <blancont@rose-hulman.edu>
2022-05-13 16:19:37 -0400

    refactor switch statement out of Card

--- src/main/java/datasource/CardType.java
+++ src/main/java/datasource/CardType.java
@@ -5,50 +5,73 @@ import system.cardEffects.*;
 public enum CardType {
 
     ATTACK(Messages.getMessage(Messages.ATTACK_CARD),
-            new AttackEffect()),
+            new AttackEffect(),
+            Messages.ATTACK_DESC),
     EXPLODING_KITTEN(Messages.getMessage(Messages.EXPLODING_KITTEN_CARD),
-            null),
+            null,
+            Messages.EXPLODING_DESC),
     DEFUSE(Messages.getMessage(Messages.DEFUSE_CARD),
-            null),
+            null,
+            Messages.DEFUSE_DESC),
     SKIP(Messages.getMessage(Messages.SKIP_CARD),
-            new SkipEffect()),
+            new SkipEffect(),
+            Messages.SKIP_DESC),
     FAVOR(Messages.getMessage(Messages.FAVOR_CARD),
-            null),
+            null,
+            Messages.FAVOR_DESC),
     SHUFFLE(Messages.getMessage(Messages.SHUFFLE_CARD),
-            new ShuffleEffect()),
+            new ShuffleEffect(),
+            Messages.SHUFFLE_DESC),
     BEARD_CAT(Messages.getMessage(Messages.BEARD_CAT_CARD),
-            null),
+            null,
+            Messages.BEARD_CAT_DESC),
     TACO_CAT(Messages.getMessage(Messages.TACO_CAT_CARD),
-            null),
+            null,
+            Messages.TACO_CAT_DESC),
     HAIRY_POTATO_CAT(Messages.getMessage(Messages.HAIRY_POTATO_CAT),
-            null),
+            null,
+            Messages.HAIRY_POTATO_CAT_DESC),
     RAINBOW_RALPHING_CAT(Messages.getMessage(Messages.RAINBOW_RALPHING_CAT),
-            null),
+            null,
+            Messages.RAINBOW_CAT_DESC),
     CATTERMELON(Messages.getMessage(Messages.CATTERMELON),
-            null),
+            null,
+            Messages.CATTERMELON_DESC),
     FERAL_CAT(Messages.getMessage(Messages.FERAL_CAT),
-            null),
+            null,
+            Messages.FERAL_CAT_DESC),
     DRAW_FROM_THE_BOTTOM(Messages.getMessage(Messages.DRAW_FROM_THE_BOTTOM),
-            new DrawFromBottomEffect()),
+            new DrawFromBottomEffect(),
+            Messages.DRAW_FROM_BOTTOM_DESC),
     NOPE(Messages.getMessage(Messages.NOPE),
-            null),
+            null,
+            Messages.NOPE_DESC),
     ALTER_THE_FUTURE(Messages.getMessage(Messages.ALTER_THE_FUTURE),
-            new AlterTheFutureEffect()),
+            new AlterTheFutureEffect(),
+            Messages.ALTER_DESC),
     TARGETED_ATTACK(Messages.getMessage(Messages.TARGETED_ATTACK),
-            null),
+            null,
+            Messages.TARGETED_ATTACK_DESC),
     SEE_THE_FUTURE(Messages.getMessage(Messages.SEE_THE_FUTURE),
-            new SeeTheFutureEffect());
+            new SeeTheFutureEffect(),
+            Messages.SEE_DESC);
 
     private String displayName;
     private EffectPattern effectPattern;
+    private Messages description;
 
-    CardType(String displayNameStr, EffectPattern pattern) {
+    CardType(String displayNameStr, EffectPattern pattern, Messages desc) {
         this.displayName = displayNameStr;
         this.effectPattern = pattern;
+        this.description = desc;
     }
 
     public EffectPattern getEffectPattern() {
         return this.effectPattern;
+    }
+
+    public String getDescription() {
+        return Messages.getMessage(description);
     }
 
     @Override
--- src/main/java/system/Card.java
+++ src/main/java/system/Card.java
@@ -1,72 +1,12 @@
 package system;
 
 import datasource.CardType;
-import datasource.Messages;
 
 public class Card {
     private CardType cardType;
-    private Messages description;
 
     public Card(CardType type) {
         this.cardType = type;
-        this.setDescription();
-    }
-    public void setDescription() {
-        switch (this.cardType) {
-            case ALTER_THE_FUTURE:
-                this.description =  Messages.ALTER_DESC;
-                break;
-            case TARGETED_ATTACK:
-                this.description =  Messages.TARGETED_ATTACK;
-                break;
-            case SEE_THE_FUTURE:
-                this.description =  Messages.SEE_DESC;
-                break;
-            case NOPE:
-                this.description =  Messages.NOPE_DESC;
-                break;
-            case SKIP:
-                this.description =  Messages.SKIP_DESC;
-                break;
-            case FAVOR:
-                this.description =  Messages.FAVOR_DESC;
-                break;
-            case ATTACK:
-                this.description =  Messages.ATTACK_DESC;
-                break;
-            case DEFUSE:
-                this.description =  Messages.DEFUSE_DESC;
-                break;
-            case SHUFFLE:
-                this.description =  Messages.SHUFFLE_DESC;
-                break;
-            case TACO_CAT:
-                this.description =  Messages.TACO_CAT_DESC;
-                break;
-            case BEARD_CAT:
-                this.description =  Messages.BEARD_CAT_DESC;
-                break;
-            case FERAL_CAT:
-                this.description =  Messages.FERAL_CAT_DESC;
-                break;
-            case CATTERMELON:
-                this.description =  Messages.CATTERMELON_DESC;
-                break;
-            case EXPLODING_KITTEN:
-                this.description =  Messages.EXPLODING_DESC;
-                break;
-            case HAIRY_POTATO_CAT:
-                this.description =  Messages.HAIRY_POTATO_CAT_DESC;
-                break;
-            case DRAW_FROM_THE_BOTTOM:
-                this.description =  Messages.DRAW_FROM_BOTTOM_DESC;
-                break;
-            case RAINBOW_RALPHING_CAT:
-                this.description =  Messages.RAINBOW_CAT_DESC;
-                break;
-            default:
-                this.description =  Messages.EMPTY_DESC;
-        }
     }
 
     public String getName() {
@@ -108,6 +48,6 @@ public class Card {
     }
 
     public String getDesc() {
-        return Messages.getMessage(this.description);
+        return cardType.getDescription();
     }
 }
