eaee9ee9df718fad11f31304d10aa5b68bafcdac
rhit-fengr <fengr@rose-hulman.edu>
2022-05-19 08:14:41 -0400

    testCheckCatPairMatchOfMixCards2() passes

--- src/test/java/system/UnitTesting/UserUnitTesting.java
+++ src/test/java/system/UnitTesting/UserUnitTesting.java
@@ -533,5 +533,16 @@ public class UserUnitTesting {
         Assertions.assertFalse(user.checkCatPairMatch(card1,card2));
     }
 
+    @Test
+    public void
+    testCheckCatPairMatchOfMixCards2() {
+        ArrayList<Card> list = new ArrayList<>();
+        Card card1 = new Card(CardType.CATTERMELON);
+        Card card2 = new Card(CardType.ATTACK);
+        list.add(card1);
+        list.add(card2);
+        User user = new User("test1", false, list);
+        Assertions.assertFalse(user.checkCatPairMatch(card1,card2));
+    }
 
 }
