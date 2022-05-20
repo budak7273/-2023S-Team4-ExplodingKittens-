e3729198ecf5c83ccdc700a0c8651f5fdb18ae21
rhit-daviscl <daviscl@rose-hulman.edu>
2022-05-19 20:24:39 -0400

    Added second test for Feature one

--- src/test/java/system/IntegrationTesting/FeatureTesting/FeatureOneTesting.java
+++ src/test/java/system/IntegrationTesting/FeatureTesting/FeatureOneTesting.java
@@ -34,6 +34,33 @@ public class FeatureOneTesting {
             gameState.drawCardForCurrentTurn();
         }
         Assertions.assertEquals(gameState.getPlayerQueue().size(), 1);
+        Assertions.assertTrue(gameState.tryToEndGame());
+    }
+
+    @Test
+    public void testWhenMultiplePlayersInGameAndOneWins() {
+        Queue<User> users = new LinkedList<>();
+        users.add(new User("test1", true, new ArrayList<>()));
+        users.add(new User("test2", true, new ArrayList<>()));
+        users.add(new User("test3", true, new ArrayList<>()));
+        users.add(new User("test4", true, new ArrayList<>()));
+        users.add(new User("test5", true, new ArrayList<>()));
+        users.add(new User("test6", true, new ArrayList<>()));
+        users.add(new User("test7", true, new ArrayList<>()));
+        users.add(new User("test8", true, new ArrayList<>()));
+        GameDesigner gameDesigner = new GameDesigner(users, new JFrame());
+        gameDesigner.initializeGameState();
+        GamePlayer gamePlayer = gameDesigner.getGamePlayer();
+        GameState gameState = gamePlayer.getGameState();
+        DrawDeck drawDeck = gameState.getDrawDeck();
+        while(gameState.getPlayerQueue().size() > 1){
+            drawDeck.addCardToTop(new Card(CardType.EXPLODING_KITTEN));
+            User currentUser = gameState.getUserForCurrentTurn();
+            currentUser.removeCard(new Card(CardType.DEFUSE));
+            gameState.drawCardForCurrentTurn();
+        }
+        Assertions.assertEquals(gameState.getPlayerQueue().size(), 1);
+        Assertions.assertTrue(gameState.tryToEndGame());
     }
 
 
