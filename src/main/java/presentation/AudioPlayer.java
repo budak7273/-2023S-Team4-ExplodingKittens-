73f6c35f0b92a6c7241d80cc342dd5d0589c0220
Emily <boatmaee@rose-hulman.edu>
2023-03-21 14:35:43 -0400

    removed duplicated code in AudioPlayer

--- src/main/java/presentation/AudioPlayer.java
+++ src/main/java/presentation/AudioPlayer.java
@@ -10,15 +10,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class AudioPlayer {
-
     private AudioPlayer() { }
-
-    public static void playMusicOnStartup() {
+    private static void playMusic(String pathname){
         Runnable runnablePlay = new Runnable() {
             @Override
             public void run() {
                 try {
-                    File explosion = new File("src/main/resources/start.mp3");
+                    File explosion = new File(pathname);
                     FileInputStream fileInputStream =
                             new FileInputStream(explosion);
                     BufferedInputStream bufferedInputStream =
@@ -33,46 +31,13 @@ public class AudioPlayer {
         Thread playThread = new Thread(runnablePlay);
         playThread.start();
     }
-
-    public static void playExplosion() {
-        Runnable runnablePlay = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                File f = new File("src/main/resources/explodingKitten.mp3");
-                FileInputStream fileInputStream =
-                        new FileInputStream(f);
-                BufferedInputStream bufferedInputStream =
-                        new BufferedInputStream(fileInputStream);
-                Player player = new Player(bufferedInputStream);
-                player.play();
-            } catch (JavaLayerException | IOException e) {
-                System.err.println(Messages.getMessage(Messages.NO_MUSIC));
-            }
-        }
-        };
-        Thread playThread = new Thread(runnablePlay);
-        playThread.start();
+    public static void playMusicOnStartup() {
+      playMusic("src/main/resources/start.mp3");
     }
-
+    public static void playExplosion() {
+      playMusic("src/main/resources/explodingKitten.mp3");
+    }
     public static void playDefused() {
-        Runnable runnablePlay = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    File f = new File("src/main/resources/defused.mp3");
-                    FileInputStream fileInputStream =
-                            new FileInputStream(f);
-                    BufferedInputStream bufferedInputStream =
-                            new BufferedInputStream(fileInputStream);
-                    Player player = new Player(bufferedInputStream);
-                    player.play();
-                } catch (JavaLayerException | IOException e) {
-                    System.err.println(Messages.getMessage(Messages.NO_MUSIC));
-                }
-            }
-        };
-        Thread playThread = new Thread(runnablePlay);
-        playThread.start();
+       playMusic("src/main/resources/defused.mp3");
     }
 }
