9a4fb07ac518902900a7f645e0a5e18ec90d8796
Robb <computerguy440+gh@gmail.com>
2023-04-21 22:02:44 -0400

    Checkstyle

--- src/test/java/system/UnitTesting/UserUnitTesting.java
+++ src/test/java/system/UnitTesting/UserUnitTesting.java
@@ -484,8 +484,8 @@ public class UserUnitTesting {
 
         GameWindow gw = EasyMock.createMock(GameWindow.class);
         EasyMock.expect(gw.inputForStealCard(targetUser)).andReturn(0);
-        gw.updateEventHistoryLog("Event Log contents for player: \n" +
-                                 " took a Attack from  via a Favor\n");
+        gw.updateEventHistoryLog("Event Log contents for player: \n"
+                                 + " took a Attack from  via a Favor\n");
         EasyMock.expectLastCall();
         DrawDeck deckMock = EasyMock.createMock(DrawDeck.class);
 
